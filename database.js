e6025166abe6abd59a1f07def576c8b137b252c1
carl-vanman <70966062+carl-vanman@users.noreply.github.com>
2021-04-26 10:41:26 +0200

    Heroku access

--- database.js
+++ database.js
@@ -1,13 +1,13 @@
 const admin = require("firebase-admin");
 
 let serviceAccount;
-// HEROKU
-// heroku.com, configvars -> key = PRIVATE_KEY, VALUE -> All den hemliga infon från private key
-//if( process.env.PRIVATE_KEY ){
-	//serviceAccount = JSON.parse( process.env.PRIVATE_KEY )
-//} else {
-//	serviceAccount = require("./Firebase-private-key.json")
-//}
+HEROKU
+heroku.com, configvars -> key = PRIVATE_KEY, VALUE -> All den hemliga infon från private key
+if( process.env.PRIVATE_KEY ){
+	serviceAccount = JSON.parse( process.env.PRIVATE_KEY )
+} else {
+	serviceAccount = require("./Firebase-private-key.json")
+}
 
 const serviceAccount = require("./Firebase-private-key.json");
 
